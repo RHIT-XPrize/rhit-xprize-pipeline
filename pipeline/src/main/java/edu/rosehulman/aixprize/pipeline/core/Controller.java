b0604fc80278220f17b86668e2f3ec45305a6afc
Lewis Kelley <kelleyld@rose-hulman.edu>
2017-10-07 13:57:55 -0400

    Initial structure (#10)
    * Added .gitignore
    
    * Added Gradle to .gitignore
    
    * Set up main method.
    
    * Fleshed out the README
    
    * Updated README with working installation instructions.
    
    * Promoted Troubleshooting to a top-level heading
    
    * Added ROSJava as a listed dependency
    
    * Improved README.org
    
    * More improvements to README.org

---
+++ pipeline/src/main/java/edu/rosehulman/aixprize/pipeline/core/Controller.java
@@ -0,0 +1,19 @@
+package edu.rosehulman.aixprize.pipeline.core;
+
+import org.apache.commons.logging.Log;
+import org.ros.namespace.GraphName;
+import org.ros.node.*;
+
+public class Controller extends AbstractNodeMain {
+
+	@Override
+	public GraphName getDefaultNodeName() {
+		return GraphName.of("pipeline/core");
+	}
+	
+	@Override
+	public void onStart(ConnectedNode connectedNode) {
+		Log log = connectedNode.getLog();
+		log.info("Hello world!");
+	}
+}
