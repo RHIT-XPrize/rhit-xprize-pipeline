2eb9c68140d0db7ac3dcecd2515afd410523baa9
Lewis-Kelley <kelleyld@rose-hulman.edu>
2017-10-08 12:19:10 -0400

    Added org.json to Gradle dependencies.

--- pipeline/src/main/java/edu/rosehulman/aixprize/pipeline/annotators/ColorsAnnotator.java
+++ pipeline/src/main/java/edu/rosehulman/aixprize/pipeline/annotators/ColorsAnnotator.java
@@ -2,26 +2,45 @@ package edu.rosehulman.aixprize.pipeline.annotators;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
 
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.json.JsonCasSerializer;
-
+import org.json.*;
 
 public class ColorsAnnotator extends JCasAnnotator_ImplBase {
 
 	@Override
 	public void process(JCas cas) throws AnalysisEngineProcessException {
 		try {
-			StringWriter serialized = new StringWriter();
-			JsonCasSerializer.jsonSerialize(cas.getCas(), serialized);
-			
 			Socket socket = new Socket("127.0.0.1", 8080);
-			socket.getOutputStream().write(serialized.toString().getBytes());
+			transmitCas(cas.getCas(), socket);
+			List<Annotation> annotations = receiveAnnotations(socket);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	private void transmitCas(CAS cas, Socket socket) throws IOException {
+		StringWriter serialized = new StringWriter();
+		JsonCasSerializer.jsonSerialize(cas, serialized);
+		
+		socket.getOutputStream().write(serialized.toString().getBytes());
+	}
+	
+	private List<Annotation> receiveAnnotations(Socket socket) throws IOException {
+		BufferedReader reader 
+			= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		String line;
+		
+		while ((line = reader.readLine()) != null) {
+			JSONObject object = new JSONObject(line);
+		}
+		
+		return null;
+	}
 }
