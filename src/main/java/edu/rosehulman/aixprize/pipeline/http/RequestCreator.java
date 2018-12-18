2b91ea046335e8f202cd10a086c462b3b2641fee
SUVARNA-W541\suvarna <suvarna.adit@gmail.com>
2018-12-17 21:35:57 -0500

    fixed some bugs temporarily

--- src/main/java/edu/rosehulman/aixprize/pipeline/http/RequestCreator.java
+++ src/main/java/edu/rosehulman/aixprize/pipeline/http/RequestCreator.java
@@ -2,34 +2,55 @@ package edu.rosehulman.aixprize.pipeline.http;
 
 import java.io.*;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 import java.util.Map.Entry;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.*;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.json.JsonCasSerializer;
 
 class RequestCreator {
-	private URI uri;
+	private String uri;
 	private JCas cas;
 	private MultipartEntityBuilder multipartBuilder;
 
-	public RequestCreator(URI uri, JCas cas) {
+	public RequestCreator(String uri, JCas cas) {
 		this.uri = uri;
 		this.cas = cas;
 	}
 
 	public HttpUriRequest createRequest() throws IOException {
 		multipartBuilder = MultipartEntityBuilder.create();
-
-		RequestBuilder requestBuilder = RequestBuilder.post(uri);
 		
+		RequestBuilder requestBuilder = RequestBuilder.post(uri);
+//		URL oracle = new URL("http://localhost:56814/api/values");
+//        URLConnection yc = oracle.openConnection();
+//        BufferedReader in = new BufferedReader(new InputStreamReader(
+//                                    yc.getInputStream()));
+//        String inputLine;
+//        while ((inputLine = in.readLine()) != null) 
+//            System.out.println(inputLine);
+//        in.close();
 		requestBuilder.setEntity(createEntity());
+//		HttpClient client = new DefaultHttpClient();
+//		HttpPost post = new HttpPost("http://localhost:56814/api/values");
+//
+//		StringEntity input = new StringEntity("pf you");
+//		input.setContentType("multipart/form-data");
+//		post.setEntity(input);
+//		HttpResponse response = client.execute(post);
+//		System.out.println(response.getStatusLine()); 
 		return requestBuilder.build();
 	}
 
@@ -51,8 +72,8 @@ class RequestCreator {
 	private Map<String, byte[]> readBinaries(JCas cas) {
 		Map<String, byte[]> binaries = new HashMap<>();
 
-		//addCurrentSofa(cas, binaries);
-		//addViewSofas(cas, binaries);
+		addCurrentSofa(cas, binaries);
+		addViewSofas(cas, binaries);
 
 		return binaries;
 	}
