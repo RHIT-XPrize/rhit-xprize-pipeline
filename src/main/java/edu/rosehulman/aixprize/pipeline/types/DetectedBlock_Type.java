3919f8d74052f13196cdc900e76cf71ecdd1c0cd
crowelmk <crowelmk@rose-hulman.edu>
2019-01-28 21:45:28 -0500

    Edited name of hue values for colors in POST response

--- src/main/java/edu/rosehulman/aixprize/pipeline/types/DetectedBlock_Type.java
+++ src/main/java/edu/rosehulman/aixprize/pipeline/types/DetectedBlock_Type.java
@@ -17,6 +17,9 @@ public class DetectedBlock_Type extends Annotation_Type {
 	public final static boolean featOkTst = JCasRegistry
 			.getFeatOkTst("edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
 
+	final Feature casFeat_id;
+	final int casFeatCode_id;
+	
 	final Feature casFeat_center_X;
 	final int casFeatCode_center_X;
 	
@@ -26,40 +29,52 @@ public class DetectedBlock_Type extends Annotation_Type {
 	final Feature casFeat_depth;
 	final int casFeatCode_depth;
 
-	public String getCenter_X(int addr) {
-		if (featOkTst && casFeat_center_X == null)
-			jcas.throwFeatMissing("center_X", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
-		return ll_cas.ll_getStringValue(addr, casFeatCode_center_X);
+	public int getId(int addr) {
+		if (featOkTst && casFeat_id == null)
+			jcas.throwFeatMissing("id", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
+		return ll_cas.ll_getIntValue(addr, casFeatCode_id);
 	}
 
-	public void setCenter_X(int addr, String v) {
-		if (featOkTst && casFeat_center_X == null)
-			jcas.throwFeatMissing("center_X", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
-		ll_cas.ll_setStringValue(addr, casFeatCode_center_X, v);
+	public void setId(int addr, int v) {
+		if (featOkTst && casFeat_id == null)
+			jcas.throwFeatMissing("id", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
+		ll_cas.ll_setIntValue(addr, casFeatCode_id, v);
 	}
 	
-	public String getCenter_Y(int addr) {
-		if (featOkTst && casFeat_center_Y == null)
-			jcas.throwFeatMissing("center_Y", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
-		return ll_cas.ll_getStringValue(addr, casFeatCode_center_Y);
+	public int getCenter_X(int addr) {
+		if (featOkTst && casFeat_center_X == null)
+			jcas.throwFeatMissing("center_X", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
+		return ll_cas.ll_getIntValue(addr, casFeatCode_center_X);
 	}
 
-	public void setCenter_Y(int addr, String v) {
-		if (featOkTst && casFeat_center_Y == null)
-			jcas.throwFeatMissing("center_Y", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
-		ll_cas.ll_setStringValue(addr, casFeatCode_center_Y, v);
+	public void setCenter_X(int addr, int v) {
+		if (featOkTst && casFeat_center_X == null)
+			jcas.throwFeatMissing("center_X", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
+		ll_cas.ll_setIntValue(addr, casFeatCode_center_X, v);
 	}
 	
-	public String getDepth(int addr) {
-		if (featOkTst && casFeat_depth == null)
-			jcas.throwFeatMissing("depth", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
-		return ll_cas.ll_getStringValue(addr, casFeatCode_depth);
+	public int getCenter_Y(int addr) {
+		if (featOkTst && casFeat_center_Y == null)
+			jcas.throwFeatMissing("center_Y", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
+		return ll_cas.ll_getIntValue(addr, casFeatCode_center_Y);
 	}
 
-	public void setDepth(int addr, String v) {
+	public void setCenter_Y(int addr, int v) {
+		if (featOkTst && casFeat_center_Y == null)
+			jcas.throwFeatMissing("center_Y", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
+		ll_cas.ll_setIntValue(addr, casFeatCode_center_Y, v);
+	}
+	
+	public int getDepth(int addr) {
 		if (featOkTst && casFeat_depth == null)
 			jcas.throwFeatMissing("depth", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
-		ll_cas.ll_setStringValue(addr, casFeatCode_depth, v);
+		return ll_cas.ll_getIntValue(addr, casFeatCode_depth);
+	}
+
+	public void setDepth(int addr, int v) {
+		if (featOkTst && casFeat_depth == null)
+			jcas.throwFeatMissing("depth", "edu.rosehulman.aixprize.pipeline.types.DetectedBlock");
+		ll_cas.ll_setIntValue(addr, casFeatCode_depth, v);
 	}
 
 
@@ -76,15 +91,19 @@ public class DetectedBlock_Type extends Annotation_Type {
 		super(jcas, casType);
 		casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl) this.casType, getFSGenerator());
 
-		casFeat_center_X = jcas.getRequiredFeatureDE(casType, "center_X", "uima.cas.String", featOkTst);
+		casFeat_id = jcas.getRequiredFeatureDE(casType, "id", "uima.cas.int", featOkTst);
+		casFeatCode_id = (null == casFeat_id) ? JCas.INVALID_FEATURE_CODE
+				: ((FeatureImpl) casFeat_id).getCode();
+		
+		casFeat_center_X = jcas.getRequiredFeatureDE(casType, "center_X", "uima.cas.int", featOkTst);
 		casFeatCode_center_X = (null == casFeat_center_X) ? JCas.INVALID_FEATURE_CODE
 				: ((FeatureImpl) casFeat_center_X).getCode();
 		
-		casFeat_center_Y = jcas.getRequiredFeatureDE(casType, "center_Y", "uima.cas.String", featOkTst);
+		casFeat_center_Y = jcas.getRequiredFeatureDE(casType, "center_Y", "uima.cas.int", featOkTst);
 		casFeatCode_center_Y = (null == casFeat_center_Y) ? JCas.INVALID_FEATURE_CODE
 				: ((FeatureImpl) casFeat_center_Y).getCode();
 		
-		casFeat_depth = jcas.getRequiredFeatureDE(casType, "depth", "uima.cas.String", featOkTst);
+		casFeat_depth = jcas.getRequiredFeatureDE(casType, "depth", "uima.cas.int", featOkTst);
 		casFeatCode_depth = (null == casFeat_depth) ? JCas.INVALID_FEATURE_CODE
 				: ((FeatureImpl) casFeat_depth).getCode();
 	}
