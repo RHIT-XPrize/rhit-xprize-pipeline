8853d32d28af886bd5db9ded551c10f119e0c939
crowelmk <crowelmk@rose-hulman.edu>
2019-01-28 22:29:07 -0500

    Modified pointing types to store Double values in CAS rather than strings

--- src/main/java/edu/rosehulman/aixprize/pipeline/types/Pointing_Type.java
+++ src/main/java/edu/rosehulman/aixprize/pipeline/types/Pointing_Type.java
@@ -10,60 +10,69 @@ import org.apache.uima.cas.impl.FeatureImpl;
 import org.apache.uima.cas.Feature;
 import org.apache.uima.jcas.tcas.Annotation_Type;
 
-/** 
+/**
  * Updated by JCasGen Tue Oct 17 17:10:22 EDT 2017
- * @generated */
+ * 
+ * @generated
+ */
 public class Pointing_Type extends Annotation_Type {
-  /** @generated */
-  @SuppressWarnings ("hiding")
-  public final static int typeIndexID = Color.typeIndexID;
-  /** @generated 
-     @modifiable */
-  @SuppressWarnings ("hiding")
-  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("edu.rosehulman.aixprize.pipeline.types.Pointing");
- 
-  /** @generated */
-  final Feature casFeat_confidence;
-  /** @generated */
-  final int     casFeatCode_confidence;
-  /** @generated
-   * @param addr low level Feature Structure reference
-   * @return the feature value 
-   */ 
-  public String getConfidence(int addr) {
-        if (featOkTst && casFeat_confidence == null)
-      jcas.throwFeatMissing("color", "edu.rosehulman.aixprize.pipeline.types.Color");
-    return ll_cas.ll_getStringValue(addr, casFeatCode_confidence);
-  }
-  /** @generated
-   * @param addr low level Feature Structure reference
-   * @param v value to set 
-   */    
-  public void setConfidece(int addr, String v) {
-        if (featOkTst && casFeat_confidence == null)
-      jcas.throwFeatMissing("color", "edu.rosehulman.aixprize.pipeline.types.Color");
-    ll_cas.ll_setStringValue(addr, casFeatCode_confidence, v);}
-    
-  
-
-
-
-  /** initialize variables to correspond with Cas Type and Features
+	/** @generated */
+	@SuppressWarnings("hiding")
+	public final static int typeIndexID = Pointing.typeIndexID;
+	/**
 	 * @generated
-	 * @param jcas JCas
-	 * @param casType Type 
+	 * @modifiable
 	 */
-  public Pointing_Type(JCas jcas, Type casType) {
-    super(jcas, casType);
-    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+	@SuppressWarnings("hiding")
+	public final static boolean featOkTst = JCasRegistry
+			.getFeatOkTst("edu.rosehulman.aixprize.pipeline.types.Pointing");
 
- 
-    casFeat_confidence = jcas.getRequiredFeatureDE(casType, "confidence", "uima.cas.String", featOkTst);
-    casFeatCode_confidence  = (null == casFeat_confidence) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_confidence).getCode();
+	/** @generated */
+	final Feature casFeat_confidence;
+	/** @generated */
+	final int casFeatCode_confidence;
 
-  }
+	/**
+	 * @generated
+	 * @param addr
+	 *            low level Feature Structure reference
+	 * @return the feature value
+	 */
+	public double getConfidence(int addr) {
+		if (featOkTst && casFeat_confidence == null)
+			jcas.throwFeatMissing("confidence", "edu.rosehulman.aixprize.pipeline.types.Pointing");
+		return ll_cas.ll_getDoubleValue(addr, casFeatCode_confidence);
+	}
+
+	/**
+	 * @generated
+	 * @param addr
+	 *            low level Feature Structure reference
+	 * @param v
+	 *            value to set
+	 */
+	public void setConfidece(int addr, double v) {
+		if (featOkTst && casFeat_confidence == null)
+			jcas.throwFeatMissing("confidence", "edu.rosehulman.aixprize.pipeline.types.Pointing");
+		ll_cas.ll_setDoubleValue(addr, casFeatCode_confidence, v);
+	}
+
+	/**
+	 * initialize variables to correspond with Cas Type and Features
+	 * 
+	 * @generated
+	 * @param jcas
+	 *            JCas
+	 * @param casType
+	 *            Type
+	 */
+	public Pointing_Type(JCas jcas, Type casType) {
+		super(jcas, casType);
+		casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl) this.casType, getFSGenerator());
+
+		casFeat_confidence = jcas.getRequiredFeatureDE(casType, "confidence", "uima.cas.Double", featOkTst);
+		casFeatCode_confidence = (null == casFeat_confidence) ? JCas.INVALID_FEATURE_CODE
+				: ((FeatureImpl) casFeat_confidence).getCode();
+
+	}
 }
-
-
-
-    
